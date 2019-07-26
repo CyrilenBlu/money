9ef1c6807e03fbd23bc196d68231686351662180
BuildTools <unconfigured@null.spigotmc.org>
2019-07-26 22:25:51 +0200

    Configured tomcat for containerisation

--- money-client/src/main/java/blu/money/MoneyClientApplication.java
+++ money-client/src/main/java/blu/money/MoneyClientApplication.java
@@ -2,11 +2,18 @@ package blu.money;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class MoneyClientApplication {
+public class MoneyClientApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MoneyClientApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MoneyClientApplication.class, args);
--- money-server/src/main/java/blu/money/MoneyServerApplication.java
+++ money-server/src/main/java/blu/money/MoneyServerApplication.java
@@ -2,11 +2,18 @@ package blu.money;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class MoneyServerApplication {
+public class MoneyServerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MoneyServerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MoneyServerApplication.class, args);
